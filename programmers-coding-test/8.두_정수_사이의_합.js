87fee7fe0eaeda5197ce76ae2e8c8b177951112d
Jiwoo Jang <104891203+jellyjw@users.noreply.github.com>
2022-09-20 13:36:22 +0900

    Lv.1

---
+++ programmers-coding-test/8.두_정수_사이의_합.js
@@ -0,0 +1,16 @@
+//두 정수 a, b가 주어졌을 때 a와 b 사이에 속한 모든 정수의 합을 리턴하는 함수, solution을 완성하세요.
+//예를 들어 a = 3, b = 5인 경우, 3 + 4 + 5 = 12이므로 12를 리턴합니다.
+
+const solution = (a, b) => {
+  let answer = 0;
+  if (a <= b) {
+    for (let i = a; i <= b; i++) {
+      answer += i;
+    }
+  } else {
+    for (let i = b; i <= a; i++) {
+      answer += i;
+    }
+  }
+  return answer;
+};
