bd52b4c184ff374e5440381f7fde5d99aaa8d7d6
Jiwoo Jang <104891203+jellyjw@users.noreply.github.com>
2022-10-03 17:15:03 +0900

    react

--- App.js
+++ App.js
@@ -32,11 +32,16 @@ export default function App() {
     setValue("");
   };
 
+  const handleRemoveClick = () => {
+    setTodoData([]);
+  };
+
   return (
     <div className="flex items-center justify-center w-screen h-screen bg-blue-50">
       <div className="w-full p-6 m-4 bg-white rounded shadow lg:w-3/4 lg:max-w-lg">
         <div className="flex justify-between mb-3">
           <h1>할 일 목록</h1>
+          <button onClick={handleRemoveClick}>Delete All</button>
         </div>
         <Lists
           handleClick={handleClick}
