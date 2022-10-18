ed3bb21fac839dc884d0a66a1c2077faef5eb355
Jiwoo Jang <104891203+jellyjw@users.noreply.github.com>
2022-10-18 13:47:14 +0900

    Lv.1

---
+++ 17.같은_숫자는_싫어.js
@@ -0,0 +1,9 @@
+// 배열 arr가 주어집니다. 배열 arr의 각 원소는 숫자 0부터 9까지로 이루어져 있습니다. 이때, 배열 arr에서 연속적으로 나타나는 숫자는 하나만 남기고 전부 제거하려고 합니다.
+
+function solution(arr) {
+  var answer = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] !== arr[i + 1]) answer.push(arr[i]);
+  }
+  return answer;
+}
