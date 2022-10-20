f8bc0744885355def5ca3f353986a9e1a01cd1f0
Jiwoo Jang <104891203+jellyjw@users.noreply.github.com>
2022-10-20 19:56:05 +0900

    react-test-app
    App.js에 'learn react' 문구가 있는지 확인하기 위해 screen 객체 사용,
    npm test를 이용하여 test해보기

--- App.js
+++ App.js
@@ -1,55 +1,25 @@
-import React, { useState, useCallback } from "react";
-import "./App.css";
-import Form from "./components/Form";
-import Lists from "./components/Lists";
+import logo from './logo.svg';
+import './App.css';
 
-export default function App() {
-  console.log("app component");
-  //첫번째 인수 todoData는 변수의 이름이고, 두번째 인수 setTodoData는 state를 정하는 함수이다.
-  const [todoData, setTodoData] = useState([]);
-  const [value, setValue] = useState("");
-
-  const handleClick = useCallback(
-    (id) => {
-      let newTodoData = todoData.filter((data) => data.id !== id);
-      setTodoData(newTodoData);
-    },
-    [todoData]
-  );
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-
-    //새로운 할 일 데이터
-    let newTodo = {
-      id: Date.now(),
-      title: value,
-      completed: false,
-    };
-
-    //원래 있던 할 일에 새로운 할 일 더해주기
-    setTodoData((prev) => [...prev, newTodo]);
-    setValue("");
-  };
-
-  const handleRemoveClick = () => {
-    setTodoData([]);
-  };
-
+function App() {
   return (
-    <div className="flex items-center justify-center w-screen h-screen bg-blue-50">
-      <div className="w-full p-6 m-4 bg-white rounded shadow lg:w-3/4 lg:max-w-lg">
-        <div className="flex justify-between mb-3">
-          <h1>할 일 목록</h1>
-          <button onClick={handleRemoveClick}>Delete All</button>
-        </div>
-        <Lists
-          handleClick={handleClick}
-          todoData={todoData}
-          setTodoData={setTodoData}
-        />
-        <Form handleSubmit={handleSubmit} value={value} setValue={setValue} />
-      </div>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
+
+export default App;
